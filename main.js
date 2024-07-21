52f40fdbfe237c6463d10987b703af9e15f8a77b
Amirkhon160 <amirkhoninagamov001@gmail.com>
2024-07-21 16:45:12 +0500

    uyga vazifa

---
+++ main.js
@@ -0,0 +1,29 @@
+let userName = prompt("Ismingizni kiriting");
+console.log(userName);
+
+let surname = prompt("Familiyangizni kiriting");
+console.log(surname);
+
+let age = prompt("Yoshiz nechchida?");
+console.log(age);
+
+let favlesson = prompt("Yoqtirgan faningiz");
+console.log(favlesson);
+
+let address = prompt("Qayerda yashaysiz?");
+console.log(address);
+
+let fbplayer = prompt("Siznin eng yoqtirgan futbol oyinchingiz");
+console.log(fbplayer);
+
+let fear = prompt("Nimadan qorqasiz?");
+console.log(fear);
+
+let favbook = prompt("Yoqtirgan kitobingiz");
+console.log(favbook);
+
+let sleep = prompt("Kuniga nech soat uxlisiz?");
+console.log(sleep);
+
+let favfilm = prompt("Yoqtirgan filmingiz");
+console.log(favfilm);
